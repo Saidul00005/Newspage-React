f7deaae5cb4e589d31fc3bf8d5a8a82413bdbd10
Mohammad Sayadul Hoque <Saidul1000005@gmail.com>
2024-01-01 01:06:01 +0600

    Addition of "Author", "time" and "News Source".

--- src/Components/News.js
+++ src/Components/News.js
@@ -75,7 +75,7 @@ export class News extends Component {
             {!this.state.loading && this.state.articles.map((element)=>{
               return <div className="col-md-3" key={element.url} >
               <Newsitem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage}
-              newsUrl={element.url}/>
+              newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
               </div>
             })}
         </div>
