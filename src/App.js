b8c3843cca4d3a4b51fdde0bd149324d8e637fa6
Mohammad Sayadul Hoque <Saidul1000005@gmail.com>
2024-01-01 01:31:12 +0600

    Code refactorization of "News" component.

--- src/App.js
+++ src/App.js
@@ -10,21 +10,22 @@ import {
 
 
 export class App extends Component {
+  pageSize = 10 ;
   render() {
     return (
         <Router>
           <div>
               <Navbar/>
               <Routes>
-                <Route exact path="/" element={<News key="general" pageSize={20} country="in" catagory="general" />}/>
-                <Route exact path="/about" element={<News key="" pageSize={20} country="in" catagory="general" />}/>
-                <Route exact path="/business" element={<News key="business" pageSize={20} country="us" catagory="business" />}/>
-                <Route exact path="/entertainment" element={<News key="entertainment" pageSize={20} country="in" catagory="entertainment" />}/>
-                <Route exact path="/general" element={<News key="general" pageSize={20} country="in" catagory="general" />}/>
-                <Route exact path="/health" element={<News key="health" pageSize={20} country="in" catagory="health" />}/>
-                <Route exact path="/science" element={<News key="science" pageSize={20} country="in" catagory="science" />}/>
-                <Route exact path="/sports" element={<News key="sports" pageSize={20} country="us" catagory="sports" />}/>
-                <Route exact path="/technology" element={<News key="technology" pageSize={20} country="in" catagory="technology" />}/>
+                <Route exact path="/" element={<News key="general" pageSize={this.pageSize} country="in" catagory="general" />}/>
+                <Route exact path="/about" element={<News key="" pageSize={this.pageSize} country="in" catagory="general" />}/>
+                <Route exact path="/business" element={<News key="business" pageSize={this.pageSize} country="us" catagory="business" />}/>
+                <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="in" catagory="entertainment" />}/>
+                <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country="in" catagory="general" />}/>
+                <Route exact path="/health" element={<News key="health" pageSize={this.pageSize} country="in" catagory="health" />}/>
+                <Route exact path="/science" element={<News key="science" pageSize={this.pageSize} country="in" catagory="science" />}/>
+                <Route exact path="/sports" element={<News key="sports" pageSize={this.pageSize} country="us" catagory="sports" />}/>
+                <Route exact path="/technology" element={<News key="technology" pageSize={this.pageSize} country="in" catagory="technology" />}/>
               </Routes>
               
            </div>
--- src/Components/News.js
+++ src/Components/News.js
@@ -23,9 +23,9 @@ export class News extends Component {
             page:1
         }
     }
-
-    async componentDidMount(){
-       let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&catagory=${this.props.catagory}&apiKey=1d898f9052114ee3abe2a55718fe044b&pageSize=${this.props.pageSize}`
+    
+    async updateNews () {
+       const url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&catagory=${this.props.catagory}&apiKey=1d898f9052114ee3abe2a55718fe044b&page=${this.state.page}pageSize=${this.props.pageSize}`
        this.setState({loading:true});
        let data = await fetch(url) 
        let parsedData = await data.json()
@@ -33,36 +33,23 @@ export class News extends Component {
                       totalArticles:parsedData.totalResults,
                       loading:false
                      })
+    }
+    async componentDidMount(){
+       this.updateNews()
       }
 
       handleNextClick = async ()=> {
           // console.log("next")
-          let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&catagory=${this.props.catagory}&apiKey=1d898f9052114ee3abe2a55718fe044b&page=${this.state.page +1}&pageSize=${this.props.pageSize}`
-          this.setState({loading:true});
-          let data = await fetch(url);  
-          let parsedData = await data.json()
-          
-          this.setState({
-                 page:this.state.page + 1,
-                 articles: parsedData.articles,
-                 loading:false
-  
-          })
+          this.setState({page: this.state.page + 1})
+          this.updateNews()
         }
       
       handlePreviousClick = async ()=> {
         // console.log('previous')
-        let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&catagory=${this.props.catagory}&apiKey=1d898f9052114ee3abe2a55718fe044b&page=${this.state.page-1}&pageSize=${this.props.pageSize}`
-        this.setState({loading:true});
-        let data = await fetch(url);  
-        let parsedData = await data.json()
-        this.setState({
-               page:this.state.page - 1,
-               articles: parsedData.articles,
-               loading:false
-
-        })
+        this.setState({page: this.state.page - 1})
+        this.updateNews()
       }
+
   render() {
     return (
       <div className="container my-3">
