259682070c992d482823d15bfb41cb346da4420c
Mohammad Sayadul Hoque <Saidul1000005@gmail.com>
2024-01-02 16:22:52 +0600

    Addition of "Top loading bar"

--- src/Components/News.js
+++ src/Components/News.js
@@ -44,15 +44,18 @@ export class News extends Component {
    
     //  }
      async componentDidMount(){
+      this.props.setProgress(10);
       const url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1d898f9052114ee3abe2a55718fe044b&page=${this.state.page}&pageSize=${this.props.pageSize}`
       this.setState({loading:true});
       let data = await fetch(url) 
+      this.props.setProgress(30);
       let parsedData = await data.json()
+      this.props.setProgress(70);
       this.setState({articles: parsedData.articles, 
                      totalArticles:parsedData.totalResults,
                      loading:false
                     })
-
+      this.props.setProgress(100);
        }     
       // handleNextClick = async ()=> {
       //     // console.log("next")
@@ -69,7 +72,7 @@ export class News extends Component {
             
       fetchMoreData = async () => {
        this.setState({page: this.state.page + 1})
-       const url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1d898f9052114ee3abe2a55718fe044b&page=${this.state.page}&pageSize=${this.props.pageSize}`
+       const url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1d898f9052114ee3abe2a55718fe044b&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
        let data = await fetch(url) 
        let parsedData = await data.json()
        this.setState({articles: this.state.articles.concat(parsedData.articles), 
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -8,10 +8,13 @@ export class Newsitem extends Component {
       
       <div className='my-3'>
         <div className="card">
+             <div style={{display:"flex",justifyContent:"flex-end",position:"absolute",right:"0"}}>
+             <span className="badge rounded-pill bg-success">
+             {source}</span>
+             </div>
             <img src={imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">{title} <span className="position-absolute top-0 translate-middle badge rounded-pill bg-success" style={{left:"50%", zIndex:"1"}}>
-                {source}</span></h5>
+                <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
                 <p className="card-text"><small className="text-body-secondary">By {author? "Unknown":author} on {new Date(date).toGMTString()}</small></p>
                 <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
