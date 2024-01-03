d73e935d597e26baac2802c1fbf94dbd859aa51d
Mohammad Sayadul Hoque <Saidul1000005@gmail.com>
2024-01-03 19:30:33 +0600

    Change to "Function based components" from "Class based components"

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react'
+import React from "react";
 
-export class Newsitem extends Component {
-    
-  render() {
-    let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
-    return (
-      
-      <div className='my-3'>
-        <div className="card">
-             <div style={{display:"flex",justifyContent:"flex-end",position:"absolute",right:"0"}}>
-             <span className="badge rounded-pill bg-success">
-             {source}</span>
-             </div>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <p className="card-text"><small className="text-body-secondary">By {author? "Unknown":author} on {new Date(date).toGMTString()}</small></p>
-                <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
-            </div>
+const Newsitem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className="badge rounded-pill bg-success">{source}</span>
+        </div>
+        <img src={imageUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              By {author ? "Unknown" : author} on {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn-dark"
+          >
+            Read More
+          </a>
         </div>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
-export default Newsitem
+export default Newsitem;
